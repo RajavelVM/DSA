4140e752a235bc8483fecc03ad8824cbd56fe214
--global <rajavel1772004@gmail.com>
2024-01-19 00:14:50 +0530

    if loop

--- DAY 2/Demo.java
+++ DAY 2/Demo.java
@@ -4,8 +4,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Enter a number");
         Scanner in =new Scanner(System.in);
-        int n=in.nextInt();
-        for (int num=1; num<=100; num+=2){
+        int n =in.nextInt();
+        for (int num=1; num<=n; num+=2){
             System.out.println("hey cutie");
         }
     }
