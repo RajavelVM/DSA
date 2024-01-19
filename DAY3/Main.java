7d49deef7272cbb0843941fc99fa19f54ea269c9
--global <rajavel1772004@gmail.com>
2024-01-19 12:44:06 +0530

    Largest num program

---
+++ DAY3/Main.java
@@ -0,0 +1,35 @@
+package DAY3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        in.close();
+        
+
+        int max =a;
+        if (b>max){
+            max=b;
+        }
+        if (c>max){
+            max=c;
+        }
+        System.out.println(max);
+
+    }
+}
+
+    
+
+
+
+    
+
+    
+
