56b2bfb1dcd9d78bf49238da0249a38eb0f50e9f
--global <rajavel1772004@gmail.com>
2024-01-20 01:30:01 +0530

    reversing given number

---
+++ DAY4/Reverse.java
@@ -0,0 +1,17 @@
+package DAY4;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int num=95667;
+        int ans=0;
+
+        while (num>0) {
+            int rem =num%10;
+            num=num/10;
+
+            ans=ans*10+rem;
+        }
+        System.out.println(ans);
+    }
+    
+}
