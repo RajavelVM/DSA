3e1be76e0ffae48a992ed60a3c4b8140af53af24
--global <rajavel1772004@gmail.com>
2024-01-19 14:48:17 +0530

    loops

--- DAY2/Demo.java
+++ DAY2/Demo.java
@@ -6,8 +6,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Enter a number");
         Scanner in =new Scanner(System.in);
-        in.close();
         int n =in.nextInt();
+        in.close();
         for (int num=1; num<=n; num+=2)
         {
             System.out.println("hey cutie");
