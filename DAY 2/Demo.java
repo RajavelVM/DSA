e860ac08640ba483dd0de052baa04d3fac5e99f8
--global <rajavel1772004@gmail.com>
2024-01-19 12:43:29 +0530

    in.close()learnt

--- DAY 2/Demo.java
+++ DAY 2/Demo.java
@@ -1,11 +1,15 @@
+
 import java.util.Scanner;
+
 
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Enter a number");
         Scanner in =new Scanner(System.in);
+        in.close();
         int n =in.nextInt();
-        for (int num=1; num<=n; num+=2){
+        for (int num=1; num<=n; num+=2)
+        {
             System.out.println("hey cutie");
         }
     }
