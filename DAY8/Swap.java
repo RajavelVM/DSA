2ec6e3dc6421ef664f820f5dd81d87c41acc2ea7
--global <rajavel1772004@gmail.com>
2024-01-29 23:54:56 +0530

    Arrays and arrays list (swap program)

---
+++ DAY8/Swap.java
@@ -0,0 +1,19 @@
+package DAY8;
+
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr ={1,3,5,9,99,999};
+        swap(arr,1,3);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+
+static void swap(int[] arr,int index1,int index2){
+    int temp=arr[index1];
+    arr[index1]=arr[index2];
+    arr[index2]=temp;
+
+}
+}
